e0a48b961e9712136abcd18ae19d2a7ad15ed544
Barbosa885 <gustavosports7@gmail.com>
2022-04-18 02:17:38 -0300

    add the powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+function android {emulator -avd master}
+function masterFront {cd "C:\Users\gusta\Documents\Projects\master-front"}
+function masterBack {cd "C:\Users\gusta\Documents\Projects\master-back"}
+function projetos {cd "C:\Users\gusta\Documents\Projects"}
