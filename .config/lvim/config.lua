dd1b767d86ad5ab2b3f6b696447c0f981a756db2
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-06-29 18:12:37 -0300

    Updates config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -9,7 +9,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "kanagawa"
+lvim.colorscheme = "tokyonight"
 -- lvim.lsp.diagnostics.virtual_text = false
 -- vim.g.tokyonight_style = "night"
 
@@ -59,7 +59,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
