cb737e303af19aa4769c9506dc8e4f73bc1b45c3
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-07-04 01:35:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,3 +38,15 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 # install plugins
 nvim --headless +PackerSync +qa 
+
+# github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt install gh
+
+# log into github with gh
+gh auth login
