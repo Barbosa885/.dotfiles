9bfe1a8ea8607b1070f2c92a5f365e51bb1771cc
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-07-05 18:18:52 -0300

    Make the install script more visual when executed

--- install.sh
+++ install.sh
@@ -1,43 +1,64 @@
 # install nix
+echo "🚀 Installing nix"
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
 # source nix 
 . ~/.nix-profile/etc/profile.d/nix.sh
 
+echo "🚀 Installing packages"
 # install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.antibody \
-	nixpkgs.neovim \
-	nixpkgs.gcc \
-	nixpkgs.yarn \
-	nixpkgs.bat \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.git 
+packages=(
+  zsh 
+  antibody
+  neovim 
+  gcc 
+  yarn 
+  bat 
+  tmux 
+  stow 
+  git 
+)
+
+for package in ${packages[@]}
+do 
+  echo "📦 Installing $package"
+  nix-env -iA nixpkgs.$package
+done
 
 # stow files
-stow zsh
-stow tmux
-stow git 
-stow nvim
-stow p10k
+stow_dirs=(
+  zsh
+  tmux
+  git
+  nvim
+  p10k
+)
+
+for dirs in ${stow_dirs[@]}
+do
+  echo "➦ Creating symlinks for $dirs"
+  stow $dirs
+done
+  
 
 # add zsh as a login shell
+echo "🚀 Configuring ZSH as default shell"
 command -v zsh | sudo tee -a /etc/shells
-
-# use zsh as my default shell
 chsh -s $(which zsh) $USER
 
-# load zsh
-exec zsh
-
 # bundle zsh plugins
+echo "🎁 Bundle antibody plugins"
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
 # install packer
+echo "🚀 Installing nvim plugin manager"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 	~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # install plugins
+echo "🔨 Installing nvim plugins"
 nvim --headless +PackerSync +qa 
+
+# execute zsh
+echo "⚙️  Executing zsh"
+exec zsh
